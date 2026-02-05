472203980569603adcd6e419ac0a060ea7150bb9
asaf_ashwal <ashwalasafx@gmail.com>
2026-02-05 17:19:20 +0200

    done without bonus

--- gameData.js
+++
@@ -1,4 +0,0 @@
-// export let players = [{ name: 'player1',score: 0 }, { name: 'player2',score: 0 }];
-// export let currentPlayer = 0;
-// export let currentScore = 0;
-// export let maxScore = 100;
